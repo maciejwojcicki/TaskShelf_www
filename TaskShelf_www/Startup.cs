414cc56a1f212a131682677872e2fcbf1d77000d
maciejwojcicki <maciej.wojcicki@multimedialab.pl>
2015-04-04 17:34:20 +0200

    initialize

---
+++ TaskShelf_www/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TaskShelf_www.Startup))]
+namespace TaskShelf_www
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
