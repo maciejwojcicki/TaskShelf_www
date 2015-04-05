82e4162cfc02dc3f84ad6e0e42868ca16321cf21
deith2 <czarnuch1001@gmail.com>
2015-04-05 17:18:23 +0200

    Opis: Poprawa błędu Html.Helper extension w Web.config

--- TaskShelf_www/App_Start/RouteConfig.cs
+++ TaskShelf_www/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TaskShelf_www
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "User", action = "Loginbox", id = UrlParameter.Optional }
             );
         }
     }
